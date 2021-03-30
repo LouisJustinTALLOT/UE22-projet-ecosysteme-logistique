20896cedb5873ac8bd5d0b13d9a83fc1c182e9ba
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-03-30 14:30:43 +0200

    traitement rapide des 1.7 Go de la base SIRENE en C++

---
+++ tests/traitement_base_SIRENE/traitement_base_sirene.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+int main()
+{
+    ////////////////// lecture du fichier source //////////////////
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    std::string input_filename = "base-sirene.json";
+    std::string output_filename = "base_sirene_shortened_json_cpp.json";
+
+    std::ifstream file_in(input_filename);
+
+    std::string input_text;
+
+    start = std::chrono::high_resolution_clock::now();
+    getline(file_in, input_text);
+
+    end = std::chrono::high_resolution_clock::now();
+    file_in.close();
+
+    std::cout << "lecture finie en ";
+    std::cout << (end - start).count() / 1000000000.0 << " secondes" << std::endl;
+
+    ////////////////// début du traitement //////////////////
+    start = std::chrono::high_resolution_clock::now();
+    /*
+     * traitement fini en 62 secondes en faisant auto car : input_text
+     * et en 52 secondes en faisant int i = 0; i < input_text.size(); i++
+     */
+    // std::ofstream file_out(output_filename);
+
+    std::string output_text = "[";
+    // file_out << output_text;
+    int nombre_entrees = 0;
+
+    int input_length = int(input_text.size());
+    std::cout << "taille du texte d'entrée : " << input_length << std::endl;
+
+    for (int i = 1; i < input_length - 10; i++)
+    {
+
+        // // // output_text += input_text[i]; // ne sert plus
+
+        if (input_text.substr(i, 8) == "recordid")
+        {
+            output_text += "{";
+            // file_out << "{";
+            nombre_entrees++;
+            i += 750;
+        }
+
+        else if (input_text.substr(i, 8) == "record_t")
+        {
+            output_text += "}";
+            // file_out << "}";
+            if (i + 50 < input_length)
+            {
+                output_text += ",";
+                // file_out << ",";
+            }
+        }
+
+        // if (i > 1000)
+        // {
+        //     break;
+        // }
+    }
+    output_text += "]";
+    
+    end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "traitement fini en ";
+    std::cout << (end - start).count() / 1000000000.0 << " secondes" << std::endl;
+
+    ////////////////// écriture du résultat dans un fichier abrégé //////////////////
+    start = std::chrono::high_resolution_clock::now();
+
+    std::ofstream file_out(output_filename);
+
+    file_out << output_text;
+
+    file_out.close();
+
+    end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "écriture au fichier résultat en ";
+    std::cout << (end - start).count() / 1000000000.0 << " secondes" << std::endl;
+
+    std::cout << std::endl
+              << nombre_entrees << std::endl;
+    return 0;
+}
