d866d12fc129437e4e7006e9ffc69d68b8044bbd
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-03-31 10:59:03 +0200

    WIP : tentative de contrôler les layers

--- tests/test_POO/test_POO.js
+++ tests/test_POO/test_POO.js
@@ -13,6 +13,8 @@ let element;
 // La carte
 let map;
 
+let res;
+
 var greenIcon = new L.Icon({
     iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
     shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
@@ -41,18 +43,22 @@ var shopIcon = new L.Icon({
 });
 
 class DataSource {
-    constructor(name, url, icon) {
+    constructor(name, url, icon, id_name) {
         this.name = name;
         this.source_url = url;
         this.icon = icon;
+        this.id_name = id_name;
     }
 }
 
 function get_and_display(DataSourceObject) {
-    let current_name = DataSourceObject.name
+    // console.log(typeof (control_panel));
+
+    let current_name = DataSourceObject.name;
     console.log("Loading JSON..." + DataSourceObject.name);
     let url = DataSourceObject.source_url;
     console.log(url);
+    let res;
 
     jQuery.getJSON(url,
         function (data) {
@@ -78,11 +84,22 @@ function get_and_display(DataSourceObject) {
                 markers.addLayer(marker);
             }
             map.addLayer(markers);
-
-            console.log("Displayed");
+            // let overlay_map = {
+            //     current_name: markers
+            // }
+            //L.control.layers(null, overlay_map).addTo(map);
+            // control_panel.addOverlay(overlay_map, current_name);
+            // console.log("Displayed");
+            /*res = {
+                "name": current_name, "markers": markers
+            }
+            console.log("ici")
+            return res; */
         }
-    );
+    )
 
+    // setTimeout(() => { console.log("là"); return res; }, 400);
+    // return res;
 
 }
 
@@ -100,6 +117,24 @@ function init() {
 
     // Par Leaflet, on crée une "Carte Leaflet" sur l'élément où l'on veut afficher la carte
     map = L.map(element);
+    //var grayscale = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    //    streets   = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
+    /*
+        var map = L.map('map', {
+            center: [48.844952, 2.339193],
+            zoom: 12,
+            layers: [grayscale]
+        }); */
+
+    /* var baseMaps = {
+         "Grayscale": grayscale,
+         "Streets": streets
+     }; */
+
+    /*var overlayMaps = {
+        "Cities": cities
+    }; */
+    //L.control.layers(baseMaps, overlayMaps).addTo(map);
 
     // Tile Layer (surcouche) d'OSM (Open Street Map) : c'est de là que l'on récupère nos données
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
@@ -112,6 +147,8 @@ function init() {
 
     // On définit la vue de la carte (centrée sur les Mines, avec un zoom de 12)
     map.setView(target, 12);
+    var layer_control = new L.control.layers(null, null, { collapsed: false }).addTo(map);
+
 
     let restau_points = new DataSource(
         "Restaurants",
@@ -127,8 +164,44 @@ function init() {
         "magasins"
     );
 
+    let list_data_sources = [restau_points, shop_points];
+    let list_overlay_layers = [];
+    let data_source;
+
+    // for (data_source of list_data_sources) {
+    //     let div_checkboxes = document.getElementById("checkboxes-data-selection");
+    //     let new_input = document.createElement("input");
+    //     new_input.type = "checkbox";
+    //     new_input.id = data_source.id_name;
+    //     new_input.name = data_source.name;
+    //     new_input.onclick = toggle_display_data(data_source);
+    //
+    //     let new_label = document.createElement("label");
+    //     //new_label.for = data_source.name;
+    //     new_label.textContent = data_source.name;
+    //
+    //     div_checkboxes.append(new_input);
+    //     div_checkboxes.append(new_label);
+    // }
+
+    // setTimeout(() => {
     get_and_display(restau_points);
     get_and_display(shop_points);
+    // }, 200);
+
+    /*
+        for (data_source of list_data_sources) {
+            new_overlay_layer = get_and_display(data_source);
+            setTimeout(() => {
+                console.log(new_overlay_layer);
+    
+                layer_control.addOverlay(new_overlay_layer.markers, new_overlay_layer.name);
+            }, 500).then()      
+        } */
+
+
+}
+
 function toggle_display_data(data_source) {
     let checkbox = document.getElementById(data_source.id_name);
 
