888e9cd6ceacb72c90ea2770ffd670b544b5387c
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-04-13 14:25:08 +0200

    on change l'endroit où on output

--- src/clusterizer/Clusterizer.py
+++ src/clusterizer/Clusterizer.py
@@ -224,9 +224,12 @@ def save_to_map(centroids, hulls, path):
 def test_geojson():
     df = nettoyer(gpd.read_file("../../tests/gis/input/reducted.geojson"))
     df, centroids, hulls = clusterize(df, 10, dict=False)
-    save_to_map(centroids, hulls, "../../tests/gis/output/clusterized.html")
+    save_to_map(centroids, hulls, "output/clusterized.html")
+
+
+
 
 def test_json():
     df = nettoyer(pd.read_json("../../tests/gis/input/base_sirene_shortened_json_cpp.json"))
     df, centroids, hulls = clusterize(df, 50, dict=True)
-    save_to_map(centroids, hulls, "../../tests/gis/output/clusterized.html")
+    save_to_map(centroids, hulls, "output/clusterized.html")
