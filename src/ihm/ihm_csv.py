f7562d829ada6eab32d953a66c8c1a67a2b86586
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-06-01 11:26:31 +0200

    Début de l'ihm

---
+++ src/ihm/ihm_csv.py
@@ -0,0 +1,11 @@
+"""
+Premier ihm :utilisation d'un tableau csv pour récupérer les infos données par l'utilisateur
+Paramètres modifiables dans la fonction de clusterize : le nombre de cluster
+Paramètres modifiables souhaités en plus : encadrement du nombre de cluster, taille des cluster 
+"""
+# A faire :
+# Lire un csv
+# Récupérer les données voulues
+# Mettre un nombre de cluster automatique si non précisé
+# Lancer la fonction de clusterize avec les données choisies
+# Afficher la carte et retourner le tableau des cluster
