74f289b7ef45c53396038fca28f1497c3188d87e
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-02 17:01:29 +0200

    on récupère les données KML (Google Earth)

--- src/clusterizer/utils/seine_data_utils.py
+++ src/clusterizer/utils/seine_data_utils.py
@@ -1,5 +1,14 @@
+import re
+import pprint
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 """On va utiliser des expressions régulières pour récupérer
 les coordonnées des points sur la Seine 
 
 https://regex101.com/r/4Dnyfh/1
 """
+
+with open("../../../data/trace_seine.kml", "r", encoding="utf8") as file:
+    seine_kml = file.readlines()
