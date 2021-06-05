e80cbe42e2a8577bc9f0ef09c70840baf5f7cb6b
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-06-05 15:50:42 +0200

    lecture du tableau et recuperation des donnees

--- src/ihm/ihm_csv.py
+++ src/ihm/ihm_csv.py
@@ -3,9 +3,17 @@ Premier ihm :utilisation d'un tableau csv pour récupérer les infos données pa
 Paramètres modifiables dans la fonction de clusterize : le nombre de cluster
 Paramètres modifiables souhaités en plus : encadrement du nombre de cluster, taille des cluster 
 """
-# A faire :
-# Lire un csv
-# Récupérer les données voulues
-# Mettre un nombre de cluster automatique si non précisé
-# Lancer la fonction de clusterize avec les données choisies
-# Afficher la carte et retourner le tableau des cluster
+import csv
+
+# Lecture du tableau csv et récupération des données
+fichier = open(r"src/ihm/table_choix.csv")
+myreader = csv.reader(fichier, delimiter =';')
+
+# Pn récupére les lignes du fichier sous forme de liste de ligne
+lignes = list(myreader)
+
+fichier.close()
+
+# Les données sont sur la troisième ligne du fichier
+donnees = lignes[2]
+
