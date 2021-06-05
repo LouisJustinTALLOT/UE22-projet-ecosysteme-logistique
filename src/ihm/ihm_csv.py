d289f50ef152ac4c7664dd5ba52d1248116c5561
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-05 19:48:27 +0200

    change import name

--- src/ihm/ihm_csv.py
+++ src/ihm/ihm_csv.py
@@ -17,7 +17,7 @@ from sklearn.cluster import KMeans, MiniBatchKMeans
 
 from shapely.geometry import Polygon
 
-from src.clusterizer import Clusterizer
+from src.clusterizer import clusterizer
 from src.clusterizer.utils import clusterizer_utils
 from src.clusterizer.utils import NAF_utils
 from src.clusterizer.utils import seine_data_utils
@@ -54,7 +54,7 @@ Clusterization avec les données utlisateurs
 """
 
 print("Ouverture de la DataFrame...")
-df = Clusterizer.nettoyer(pd.read_json("../../data/base_sirene_shortened.json"))
+df = clusterizer.nettoyer(pd.read_json("../../data/base_sirene_shortened.json"))
 
 if secteur_NAF != '' :
     df = NAF_utils.filter_by_naf(df, NAF_utils.get_NAFs_by_section(secteur_NAF), "apet700")
@@ -73,7 +73,7 @@ nb_zones = 2
 liste_df = []
 for no_zone in range(nb_zones):
     liste_df.append(
-        df[no_zone == Clusterizer.rapport_a_la_seine(np.array(df.copy()["geometry"].apply(lambda x: x['coordinates'])))].reset_index(drop=True)
+        df[no_zone == clusterizer.rapport_a_la_seine(np.array(df.copy()["geometry"].apply(lambda x: x['coordinates'])))].reset_index(drop=True)
     )
 
 print("Clusterisation...")
@@ -83,15 +83,15 @@ nb_clusters_par_zone = [nb_cluster//2, nb_cluster//2]
 
 for no_zone in range(nb_zones):
     liste_df_clusters.append(
-        Clusterizer.clusterize(liste_df[no_zone], nb_clusters_par_zone[no_zone], dict=True, weight=True)
+        clusterizer.clusterize(liste_df[no_zone], nb_clusters_par_zone[no_zone], dict=True, weight=True)
     )
 
 print("Sauvegarde sur la carte...")
 
-map = Clusterizer.save_to_map(liste_df_clusters[0][1])
+map = clusterizer.save_to_map(liste_df_clusters[0][1])
 
 for no_zone in range(1, nb_zones):
-    map = Clusterizer.save_to_map(liste_df_clusters[no_zone][1], map=map)
+    map = clusterizer.save_to_map(liste_df_clusters[no_zone][1], map=map)
 
 map.save("output_ihm/clusterized_ihm.html")
 
