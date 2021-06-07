2aa974b6f447eface9322ee89cf6112f98c789f1
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-06-07 10:14:26 +0200

    ouverture du html depuis python pour tout ordi

--- src/ihm/web.py
+++ src/ihm/web.py
@@ -1,11 +1,14 @@
 import webbrowser
+import os
+
+# récupérer le chemin du répertoire courant
+path = os.getcwd()
+
 
 """
-Affichage du html depuis python, pour mon ordi
-(changer l'adresse si vous voulez que ça fonctionne pour vous)
-Je vais essayer de régler ce léger problème
+Affichage du html depuis python
+il faut être dans le répertoire ihm pour le lancer
 """
-
-adresse = "file://C:/Users/verne/Documents/Cours/Mines/S2/Informatique/Projet/UE22-projet-ecosysteme-logistique/src/ihm/output_ihm/clusterized_ihm.html"
+adresse = "file://"+path+"\output_ihm\clusterized_ihm.html"
 adresse_navig = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
 webbrowser.get(adresse_navig).open(adresse)
