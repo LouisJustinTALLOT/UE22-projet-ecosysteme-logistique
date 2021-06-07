3fe6096da67e0bade4a7f12491821ebfcbfb8c2e
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-06-07 10:23:18 +0200

    amélioration de l'ouverture html : un seul fichier a run

--- src/ihm/web.py
+++ src/ihm/web.py
@@ -1,14 +1,16 @@
 import webbrowser
 import os
 
-# récupérer le chemin du répertoire courant
-path = os.getcwd()
-
 
 """
 Affichage du html depuis python
 il faut être dans le répertoire ihm pour le lancer
 """
-adresse = "file://"+path+"\output_ihm\clusterized_ihm.html"
-adresse_navig = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-webbrowser.get(adresse_navig).open(adresse)
+def open_html(adresse) :
+    # récupérer le chemin du répertoire courant
+    path = os.getcwd()
+
+
+    adresse_totale = "file://"+path+"\\" + adresse
+    adresse_navig = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    webbrowser.get(adresse_navig).open(adresse_totale)
