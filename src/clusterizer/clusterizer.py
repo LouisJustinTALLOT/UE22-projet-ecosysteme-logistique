95f34cefcf6e0c20e558cff2440233784f561497
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-20 00:59:28 +0200

    fix bug in @JudithBellon version (#12)
    on a maintenant le bon nombre de clusters ([ 1, 37, 13,  1] par ex)
    reste à améliorer les poids des établissements

--- src/clusterizer/clusterizer.py
+++ src/clusterizer/clusterizer.py
@@ -264,12 +264,12 @@ def test_geojson():
     df, df_clusters = clusterize(df, 10, dict=False)
     save_to_map(df_clusters).save("output/INSERT_NAME.html")
 
-def nb_clusters_par_zones(liste_df, nb_clusters):
-    poids_par_zone = [0]*len(liste_df)
+def calcule_nb_clusters_par_zone(liste_df, nb_clusters):
+    poids_par_zone: np.ndarray = np.zeros(len(liste_df))
     for i in range(len(liste_df)):
         poids_par_zone[i] = clusterizer_utils.calculer_poids_cluster(liste_df[i], "apet700") 
-        poids_total = sum(poids_par_zone)
-    return np.round(poids_par_zone/poids_total * nb_clusters)
+    poids_total = np.sum(poids_par_zone)
+    return np.ceil((poids_par_zone / poids_total * nb_clusters)).astype(int)
 
 
 def main_json(rayon=8, secteur_NAF='', nb_clusters=50, adresse_map="output/clusterized_map_seine.html", reduce=False, threshold=1000):
@@ -333,7 +333,9 @@ def main_json(rayon=8, secteur_NAF='', nb_clusters=50, adresse_map="output/clust
     print("Clusterisation...", end="    ")
 
     liste_df_clusters = []
-    nb_clusters_par_zone = nb_clusters_par_zones(liste_df, nb_clusters)
+    nb_clusters_par_zone = calcule_nb_clusters_par_zone(liste_df, nb_clusters)
+    # pprint(nb_clusters_par_zone)
+
     for no_zone in range(nb_zones):
         try:
             liste_df_clusters.append(
@@ -381,6 +383,7 @@ if __name__ == "__main__":
         plt.show()
 
     else:
+        main_json(adresse_map="output/clusterized_map_improve_nb_clusters.html")
         # main_json(reduce = True, adresse_map="output/clusterized_map_optim_de_cython.html")
-        main_json(adresse_map="output/clusterized_map_IHM.html")
+        # main_json(adresse_map="output/clusterized_map_IHM.html")
         # cProfile.run('main_json(adresse_map="output/clusterized_map_optim_de_cython.html")')
