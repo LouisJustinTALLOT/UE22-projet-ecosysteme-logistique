82ec616cbe89b6930fd2221ff909976204e09888
20vernet <gabrielle.vernet@mines-paristech.fr>
2021-06-20 17:23:39 +0200

    Widget d'ouverture de carte (#7)

--- src/ihm/qt_web.py
+++ src/ihm/qt_web.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtCore import QUrl
 sys.path.append("../../")
 
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5 import QtWebEngineWidgets
 from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
 
@@ -13,23 +13,21 @@ class Interceptor(QWebEngineUrlRequestInterceptor):
     def interceptRequest(self, info):
         info.setHttpHeader(b"Accept-Language", b"en-US,en;q=0.9,es;q=0.8,de;q=0.7")
 
+class Carte(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
 
-app = QApplication.instance() 
-if not app:
-    app = QApplication(sys.argv)
-    
-fen = QMainWindow()
-fen.setWindowTitle("Ouverture HTML ?")
+        
 
-view = QtWebEngineWidgets.QWebEngineView()
-interceptor = Interceptor()
-view.page().profile().setUrlRequestInterceptor(interceptor)
-
-view.setHtml(open("output_ihm/clusterized_ihm.html", 'r').read())
+        layout_princip = QVBoxLayout()
 
 
-fen.setCentralWidget(view)
+        view = QtWebEngineWidgets.QWebEngineView()
+        interceptor = Interceptor()
+        view.page().profile().setUrlRequestInterceptor(interceptor)
 
-fen.show()
+        view.setHtml(open("output_ihm/clusterized_ihm.html", 'r').read())
+        layout_princip.addWidget(view)
 
-app.exec_()
+        self.setLayout(layout_princip)
+
