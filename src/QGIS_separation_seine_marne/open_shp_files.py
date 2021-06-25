9a611952e3c3e94b0ff918c7eaf9f7fa4b35f75a
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-25 09:48:05 +0200

    le dico du nom des zones qui partitionnent l'IDF (#3)

--- src/QGIS_separation_seine_marne/open_shp_files.py
+++ src/QGIS_separation_seine_marne/open_shp_files.py
@@ -1,6 +1,15 @@
 import geopandas as gpd
 import folium
 
+DICT_ZONES_IDF = {
+    0 : "IDF_zone_est",
+    1 : "IDF_zone_nord_ouest",
+    2 : "IDF_zone_nord",
+    3 : "IDF_zone_Paris_ouest",
+    4 : "IDF_zone_sud",
+}
+
+
 gdf = gpd.read_file("IDF_without_water.shp")
 
 map = folium.Map(
