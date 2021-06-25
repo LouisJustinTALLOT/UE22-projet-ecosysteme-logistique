fadcee13aa3a132eecc2e87992fffbd0625bd826
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-25 17:15:05 +0200

    il fallait inverser les coordonnées...

--- src/QGIS_separation_seine_marne/open_shp_files.py
+++ src/QGIS_separation_seine_marne/open_shp_files.py
@@ -1,6 +1,9 @@
 import geopandas as gpd
 import folium
 
+from shapely.geometry import Point, Polygon
+
+
 DICT_ZONES_IDF = {
     0 : "IDF_zone_est",
     1 : "IDF_zone_nord_ouest",
@@ -18,7 +21,7 @@ map = folium.Map(
     tiles="OpenStreetMap"
 )
 
-for i in DICT_ZONES_IDF.keys():
+for i in [0]:
     gdf = gpd.read_file(DIR_SHP_FILES + DICT_ZONES_IDF[i] + ".shp")
     for _, r in gdf.iterrows():
         #without simplifying the representation of each borough, the map might not be displayed
@@ -30,6 +33,28 @@ for i in DICT_ZONES_IDF.keys():
         # folium.Popup().add_to(geo_j)
         geo_j.add_to(map)
 
-map.save("test_5_zones.html")
+list_points = [
+    (48.5, 2.97),
+    (48.4, 2.3),
+    (48.38826945754673, 2.958737528893516),
+]
+
+for coord in list_points:
+    print("within : ", Point(coord[1], coord[0]).within(gdf['geometry'][0]), end=" / ")
+    print("contains : ", gdf['geometry'][0].contains(Point(coord[1], coord[0])))
+    folium.Marker(
+        location=coord, popup=f"{Point(coord[1], coord[0]).within(gdf['geometry'][0])}"
+        # location=coord, popup=f"Contains {gdf['geometry'][0].contains(Point(coord[1], coord[0]))}"
+    ).add_to(map)
+
+# print([a for a in gdf["geometry"]][0])
+
+# gpd.GeoSeries.
+
+map.save("test_point_in_zone.html")
 
 print(gdf.head())
+# gdf = gpd.read_file(DIR_SHP_FILES + DICT_ZONES_IDF[0] + ".shp")
+# # print(gdf.head())
+# print(gdf["geometry"])
+# print(Point(48.5, 2.97) in gdf["geometry"])
