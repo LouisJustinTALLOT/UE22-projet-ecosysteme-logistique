7d426a755b92c0526936e462572ec25b79ae1d0a
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-06-26 18:26:36 +0200

    transform `ihm_csv.py` into a module (#2)

--- src/ihm/ihm_csv.py
+++ src/ihm/ihm_csv.py
@@ -1,7 +1,9 @@
 """
-Premier ihm :utilisation d'un tableau csv pour récupérer les infos données par l'utilisateur
-Paramètres modifiables dans la fonction de clusterize : le nombre de cluster
-Paramètres modifiables souhaités en plus : encadrement du nombre de cluster, taille des cluster 
+Première interface Homme-machine : utilisation d'un tableau `CSV` pour récupérer les informations données par l'utilisateur
+
+Paramètres modifiables dans la fonction :code:`clusterize` : le nombre de clusters
+
+TODO : Paramètres modifiables souhaités en plus : encadrement du nombre de clusters, taille des clusters
 """
 import csv
 
@@ -17,42 +19,42 @@ sys.path.append("../../")
 from src.clusterizer import clusterizer
 from src.ihm import web
 
-
-
-print("Lecture des données...")
-# Lecture du tableau csv et récupération des données
-fichier = open(r"table_choix.csv")
-myreader = csv.reader(fichier, delimiter =';')
-
-# Pn récupére les lignes du fichier sous forme de liste de ligne
-lignes = list(myreader)
-
-fichier.close()
-
-# Les données sont sur la troisième ligne du fichier
-donnees = lignes[2]
-# Valeurs par défaut
-nb_cluster = 150
-rayon = 8
-
-if donnees[1] != '' :
-    nb_cluster = int(donnees[1])
-
-if donnees[3] != '' :
-    rayon = int(donnees[3])
-
-secteur_NAF = donnees[2]
-print("Nombre de cluster =", nb_cluster)
-print("Rayon = ", rayon)
-print("Sélection NAF : ", secteur_NAF)
-
-adresse = "output_ihm/clusterized_ihm.html"
-
-"""
-Clusterization avec les données utlisateurs
-"""
-
 if __name__ == "__main__":
+
+
+    print("Lecture des données...")
+    # Lecture du tableau csv et récupération des données
+    fichier = open(r"table_choix.csv")
+    myreader = csv.reader(fichier, delimiter =';')
+
+    # Pn récupére les lignes du fichier sous forme de liste de ligne
+    lignes = list(myreader)
+
+    fichier.close()
+
+    # Les données sont sur la troisième ligne du fichier
+    donnees = lignes[2]
+    # Valeurs par défaut
+    nb_cluster = 150
+    rayon = 8
+
+    if donnees[1] != '' :
+        nb_cluster = int(donnees[1])
+
+    if donnees[3] != '' :
+        rayon = int(donnees[3])
+
+    secteur_NAF = donnees[2]
+    print("Nombre de cluster =", nb_cluster)
+    print("Rayon = ", rayon)
+    print("Sélection NAF : ", secteur_NAF)
+
+    adresse = "output_ihm/clusterized_ihm.html"
+
+    """
+    Clusterization avec les données utlisateurs
+    """
+
 
     # On exécute le programme avec la base SIRENE :
 
