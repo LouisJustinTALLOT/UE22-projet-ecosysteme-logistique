0994bdb3d3fb5ceb8e6f5540536a2f615f8c8f15
cesar-alm <cesar_almecija@yahoo.fr>
2021-06-26 14:53:25 +0200

    Documentation for IHM #2

--- src/ihm/ihm_complet.py
+++ src/ihm/ihm_complet.py
@@ -20,8 +20,13 @@ class Wind(QMainWindow):
         self.resize(500, 500)
 
 
-    def appui_bouton_OK(self):
-        # Récupération des données de l'interface 
+    def appui_bouton_OK(self) -> None:
+        """
+        Listener pour le bouton ok.
+        Prépare les données pour lancer la clusterisation et l'affichage de la carte.
+        cf. lancement_clustering
+        """
+        # Récupération des données de l'interface
         nb_cluster_min = self.IHM.champ_nb_cluster_min.text()
         nb_cluster_max = self.IHM.champ_nb_cluster_max.text()
         secteur = self.IHM.champ_secteur.text()
@@ -49,7 +54,11 @@ class Wind(QMainWindow):
         self.lancement_clustering()
 
    
-    def lancement_clustering(self) :
+    def lancement_clustering(self)  -> None:
+        """
+        Lance la clusterisation à l'aide des paramètres entrés par l'utilisateur.
+        Ensuite, affiche la carte.
+        """
         nb_cluster_min = self.IHM._donnees[0]
         nb_cluster_max = self.IHM._donnees[1]
         rayon = self.IHM._donnees[3]
