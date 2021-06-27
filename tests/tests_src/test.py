47478fa36ed0775b242108cf0576a53c346b6b99
Judith <judith.bellon@mines-paristech.fr>
2021-06-27 15:54:30 +0200

    D'autres tests (#9)

--- tests/tests_src/test.py
+++ tests/tests_src/test.py
@@ -29,3 +29,16 @@ def test_get_NAFs_by_section():
     result = NAF_utils.get_NAFs_by_section("D")
     print(result)
     assert (result == ["35", "35.1", "35.11", "35.11Z", "35.12", "35.12Z", "35.13", "35.13Z", "35.14", "35.14Z", "35.2", "35.21", "35.21Z", "35.22", "35.22Z", "35.23", "35.23Z", "35.3", "35.30", "35.30Z" ]).all
+
+
+df_test_cl, df_test_infos = clusterizer.clusterize(df_test, 1, "geometry", dict=True)
+
+def test_numero_cluster():
+    assert (df_test_cl["cluster"] == [0, 0]).all  # les deux établissements sont dans le premier (et seul) cluster
+
+def test_get_infos_clusters_taille():
+    result = clusterizer_utils.get_infos_clusters_taille(df_test_cl)
+    assert (result == [2]).all   # un seul cluster, contenant deux établisseùents
+
+def test_centre_de_masse():
+    assert (df_test_infos["centroids"] == [{(2.348872+2.297082)/2, (48.863495+48.881103)/2}]).all
